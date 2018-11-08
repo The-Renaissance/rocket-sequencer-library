b093a3b8463cd25e290f2a755b38bb8b2c1ba3f4
The-Renaissance <nashesvobodnoye@hotmail.com>
2018-11-07 22:59:39 -0500

    Initial commit

---
+++ ROCKET_SEQUENCER.cpp
@@ -0,0 +1,227 @@
+/*
+ * CUInSpace Rocket Sequencer v0.2 (attempts at decrapifying and ditching class)
+ * It defines the basic framework for the programs that run on arduino-based rocket avionics system. 
+ * It simulates what would be called a sequencer on a real rocket, providing time frame and 
+ * starting certain actions based on a certain trigger (be it time-based or otherwise) and 
+ * oversees programs' executions.
+----------------------------------------
+ * This is still in preliminary stage. Anyone is welcome to improve it!
+ */
+ 
+
+#include "Arduino.h"
+#include "ROCKET_SEQUENCER.h"
+#undef NULL
+#define NULL 0
+
+// variable definitions
+_event_t* sequence;
+unsigned char events;
+status_t _status;
+unsigned short timer;
+unsigned long trigger_zero;//record trigger start time (in respect to program's start time)
+unsigned short countdown_time;
+int triggerPin;
+
+//*******************************
+// GET the status of the rocket
+status_t getStatus() {
+	return _status;
+}
+//*******************************
+
+//*******************************
+// set the status of the rocket (probably redundant, but whatever)
+
+void setStatus(status_t status) {
+	_status = status; // set the rocket status to trigger some event triggered by statuses
+}
+
+
+//*******************************
+
+//*******************************
+//Initializer, set trigger pin and number of events
+
+void initialize(unsigned char stage_num, int trigger_pin) {
+	events = stage_num;
+	sequence = new _event_t[stage_num];
+	setStatus(ROCKET_GROUND); // NO MISFIRE HAPPENING
+	timer = 0;
+	countdown_time = 0;
+	triggerPin = trigger_pin;
+	if (trigger_pin != -1) pinMode(trigger_pin, INPUT); // if trigger pin is set, set that pin as input
+}
+
+//*******************************
+
+//*******************************
+//Set countdown time(in milliseconds)
+void setCountdownTime(unsigned short time) {
+	countdown_time = time;
+}
+//*******************************
+
+
+
+//*******************************
+// several overloads to associate a function (returns nothing and takes
+// one/two arguments of unsigned short type/nothing at all) with the event or
+// just to set the rocket status
+
+void loadSequence(unsigned stage_num, function_2arg_t action, unsigned short arg1, unsigned short arg2,
+			  status_t status_to_trigger ) { // load a function onto the sequencer (with two arguments)
+	
+	sequence[stage_num].argc = 2;
+	sequence[stage_num].function_twoarg = action;
+	sequence[stage_num].arg1 = arg1;
+	sequence[stage_num].arg2 = arg2;
+	setStatus(status_to_trigger);
+}
+
+void loadSequence(unsigned stage_num, function_onearg_t action, unsigned short arg1,
+			  status_t status_to_trigger ){ //load a function with one argument
+	sequence[stage_num].argc = 1;
+	sequence[stage_num].function_onearg = action;
+	sequence[stage_num].arg1 = arg1;
+	setStatus(status_to_trigger);
+}
+
+void loadSequence(unsigned stage_num, function_noarg_t action,
+			  status_t status_to_trigger ){ //load a function with no argument
+	sequence[stage_num].argc = 0;
+	sequence[stage_num].function_noarg = action;
+	setStatus(status_to_trigger);
+}
+
+void loadSequence(unsigned stage_num,
+			  status_t status_to_trigger ) {//just setting a flag
+	sequence[stage_num].argc = 0;
+	sequence[stage_num].function_noarg = NULL;
+	setStatus(status_to_trigger);
+}
+
+//*******************************
+
+//*******************************
+// the function defines the condition that the function executes
+// using unsigned shorts or statuses (sorry, only one condition can be put in)
+
+void set_condition(func_compare_t cmp1, short cmp2,
+			   compare_t sign_input, unsigned stage_num) {
+	sequence[stage_num].func_to_compare = cmp1;
+	sequence[stage_num].cmp2 = cmp2;
+	sequence[stage_num].sign = sign_input;
+}
+
+void set_condition(status_t status_to_compare,
+		compare_t sign_input, unsigned stage_num) {
+	sequence[stage_num].status = status_to_compare;
+	sequence[stage_num].sign = sign_input;
+}
+//*******************************
+
+//*******************************
+//executes the action associated in each event
+void exec_event(_event_t event_name) { // execute the function predefined in loadSequence()
+					       // except when function pointer is null
+	switch(event_name.argc) {
+		case 0:
+		if (event_name.function_noarg != NULL) {(*(event_name.function_noarg)) ();}
+		break;
+		
+		case 1:
+		if (event_name.function_onearg != NULL) {(*(event_name.function_onearg)) (event_name.arg1);}
+		break;
+		
+		case 2:
+		if (event_name.function_twoarg != NULL) {(*(event_name.function_twoarg)) (event_name.arg1, event_name.arg2);}
+		break;
+		
+		default:
+		setStatus(ROCKET_ABORT); // set the abort flag if more than two arguments
+		break;
+	}
+
+}
+//*******************************
+
+//*******************************
+// the only statement to be put in loop(). other statements in the library goes to setup()
+// provides countdown and only executes crucial commands in the event of ROCKET_ABORT
+
+void start() {
+	/* countdown and trigger portion */
+	if (getStatus() == ROCKET_GROUND) {
+		if (triggerPin != -1) {
+			if (digitalRead(triggerPin) == HIGH)
+			setStatus(ROCKET_COUNTDOWN);
+			else trigger_zero = millis();
+		}
+		else setStatus(ROCKET_COUNTDOWN);
+		return;
+	}
+	else if (getStatus() == ROCKET_COUNTDOWN) {
+		if (millis() - trigger_zero >= countdown_time) setStatus(STAGE_START);
+		else {delay(1); return;}
+	}
+	
+	timer = millis() - countdown_time - trigger_zero;
+	for (unsigned char i = 0; i < events && //when rocket is not grounded/armed/in countdown
+		!(getStatus() == ROCKET_GROUND || getStatus() == ROCKET_COUNTDOWN); i++) {
+		switch (sequence[i].sign) {
+			case EQUAL:
+			if ((*(sequence[i].func_to_compare)) () == sequence[i].cmp2) {
+				if(getStatus() != ROCKET_ABORT) exec_event(sequence[i]);
+			}
+			if (_status == sequence[i].status) {
+				exec_event(sequence[i]);
+			}
+			break;
+			
+			case GREATER_THAN:
+			if ((*(sequence[i].func_to_compare)) () > sequence[i].cmp2 || 
+			    _status > sequence[i].status) {
+				if(getStatus() != ROCKET_ABORT) exec_event(sequence[i]);
+			}
+			break;
+			
+			case SMALLER_THAN:
+			if ((*(sequence[i].func_to_compare)) () < sequence[i].cmp2 || 
+			    _status < sequence[i].status) {
+				if(getStatus() != ROCKET_ABORT) exec_event(sequence[i]);
+			}
+			break;
+			
+			case GREATER_THAN_EQUAL_TO:
+			if ((*(sequence[i].func_to_compare)) () >= sequence[i].cmp2 || 
+			    _status >= sequence[i].status) {
+				if(getStatus() != ROCKET_ABORT) exec_event(sequence[i]);
+			}
+			break;
+			
+			case SMALLER_THAN_EQUAL_TO:
+			if ((*(sequence[i].func_to_compare)) () <= sequence[i].cmp2 || 
+			    _status <= sequence[i].status) {
+				if(getStatus() != ROCKET_ABORT) exec_event(sequence[i]);
+			}
+			break;
+			
+			case NOT_EQUAL:
+			if ((*(sequence[i].func_to_compare)) () != sequence[i].cmp2 || 
+			    _status != sequence[i].status) {
+				if(getStatus() != ROCKET_ABORT) exec_event(sequence[i]);
+			}
+			break;
+		}
+		
+	}
+	
+	
+	delay(1);
+}
+
+unsigned short getTimer() {
+  return timer;
+}
+//*******************************
---
+++ ROCKET_SEQUENCER.h
@@ -0,0 +1,82 @@
+/*
+ * CUInSpace Rocket Sequencer v0.2 (attempts at decrapifying and ditching class)
+ * It defines the basic framework for the programs that run on arduino-based rocket avionics system. 
+ * It simulates what would be called a sequencer on a real rocket, providing time frame and 
+ * starting certain actions based on a certain trigger (be it time-based or otherwise) and 
+ * oversees programs' executions.
+----------------------------------------
+ * This is still in preliminary stage. Anyone is welcome to improve it!
+ */
+
+#ifndef ROCKET_SEQUENCER_H
+#define ROCKET_SEQUENCER_H
+
+// this is a test comment
+typedef unsigned short (*func_compare_t) (void);
+typedef void (*function_noarg_t) (void); 		
+typedef void (*function_onearg_t) (unsigned short);
+typedef void (*function_2arg_t) (unsigned short, unsigned short);
+
+enum status_t {ROCKET_GROUND, //rocket is safed
+	      ROCKET_COUNTDOWN, //rocket is in countdown (I assume the arming will be done from the box)
+	      STAGE_ARMED,  //air-start ignition circuit moved from safe to armed
+	      STAGE_START, //air-start/first-stage ignition started
+	      PAYLOAD_START, // payload release mechanism starts
+	      ROCKET_ABORT // the launch is aborted
+}; 
+
+enum compare_t {EQUAL, NOT_EQUAL, GREATER_THAN, GREATER_THAN_EQUAL_TO,
+	      SMALLER_THAN, SMALLER_THAN_EQUAL_TO};
+
+
+
+typedef struct { // pointers to functions to call before and up to rocket's each stage startup and in each stage's duration
+	union{function_noarg_t function_noarg;
+		function_onearg_t function_onearg;
+		function_2arg_t function_twoarg;};
+	func_compare_t func_to_compare;
+	union{short cmp2; status_t status;};
+	unsigned char arg1;
+	unsigned char arg2;
+	compare_t sign;
+	status_t status_to_trigger;
+	unsigned char argc;
+} _event_t;
+
+extern _event_t* sequence;
+extern unsigned char events;
+extern status_t _status;
+extern unsigned long trigger_zero;//record rocket start time (in respect to program's start time)
+extern unsigned short countdown_time;//countdown time (in milliseconds)
+extern int triggerPin;
+extern unsigned short timer; // the rocket sequencer timer in milliseconds (both count down and count up)
+
+extern void initialize(unsigned char stages, int trigger_pin = -1); //define rocket stages and trigger pin
+extern void setCountdownTime(unsigned short time); // set countdown time(in milliseconds)
+extern void loadSequence(unsigned stage_num, function_2arg_t action, unsigned short arg1, unsigned short arg2,
+		status_t status_to_trigger = _status); // load a function onto the sequencer
+extern void loadSequence(unsigned stage_num, function_onearg_t action, unsigned short arg1,
+		status_t status_to_trigger = _status); //load a function with one argument
+extern void loadSequence(unsigned stage_num, function_noarg_t action,
+		status_t status_to_trigger = _status); //load a function with no argument
+extern void loadSequence(unsigned stage_num, 
+		status_t status_to_trigger = _status); /*
+			  		      * also, a neat feature: when combined with 
+			  		      * set_condition() and no function 
+			  		      * is defined, it will just set flag when a 
+			  		      * condition is met (e.g. a stop signal is sent during 
+			  		      * countdown due to a plane from homeland security fouling
+			  		      * the range)
+			  		      */
+extern void set_condition(func_compare_t cmp1, short cmp2,
+		compare_t sign_input, unsigned stage_num); // set the event's starting condition
+extern void set_condition(status_t status_to_compare,
+		compare_t sign_input, unsigned stage_num); //function overload for statuses
+extern void start(); // start the sequencer (THE FLIGHT COMPUTER HAS TAKEN OVER THE COUNTDOWN)
+extern status_t getStatus(); // GET the status of the rocket (as defined in enum status_t)
+extern void setStatus(status_t status); // set the status of the rocket
+extern unsigned short getTimer(); // get the time in milliseconds from the rocket timer
+
+
+
+#endif // ROCKET_SEQUENCER_H
